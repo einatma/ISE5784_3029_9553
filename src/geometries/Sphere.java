9f0647edfe414e8597fdaf9d64f747bf7fc922d6
einatma <einatmazuz@gmail.com>
2024-05-27 23:59:38 +0300

    sphere documentation

--- src/geometries/Sphere.java
+++ src/geometries/Sphere.java
@@ -2,17 +2,33 @@ package geometries;
 
 import primitives.*;
 
+/**
+ * Sphere class represents a three-dimensional sphere in 3D Cartesian coordinate system.
+ * Extends RadialGeometry.
+ */
 public class Sphere extends RadialGeometry {
     private final Point center;
 
+    /**
+     * Constructor to initialize Sphere with its center point and radius.
+     *
+     * @param c the center point of the sphere.
+     * @param r the radius of the sphere.
+     */
     public Sphere(Point c, double r) {
         super(r);
         center = c;
     }
-@Override
-  public Vector getNormal(Point p)
-{
-       return p.subtract(center).normalize();
-   }
+
+    /**
+     * Returns the normal vector to the sphere at a given point.
+     *
+     * @param p the point on the surface of the sphere.
+     * @return the normal vector at the given point.
+     */
+    @Override
+    public Vector getNormal(Point p) {
+        return p.subtract(center).normalize();
+    }
 
 }
