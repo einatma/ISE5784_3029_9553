11b14b0e98fae202504159c77fa593c7f8ca4f82
einatma <einatmazuz@gmail.com>
2024-06-23 10:31:08 +0300

    stage 6

--- unitTests/renderer/RenderTests.java
+++ unitTests/renderer/RenderTests.java
@@ -1,30 +1,27 @@
-package renderer;
+package unittests.renderer;
 
 import static java.awt.Color.*;
 
 import org.junit.jupiter.api.Test;
 
-import geometries.*;
+import geometries.Sphere;
+import geometries.Triangle;
 import lighting.AmbientLight;
 import primitives.*;
+import renderer.*;
 import scene.Scene;
 
 /**
  * Test rendering a basic image
- *
  * @author Dan
  */
 public class RenderTests {
-    /**
-     * Scene of the tests
-     */
-    private final Scene scene = new Scene("Test scene");
-    /**
-     * Camera builder of the tests
-     */
+    /** Scene of the tests */
+    private final Scene          scene  = new Scene("Test scene");
+    /** Camera builder of the tests */
     private final Camera.Builder camera = Camera.getBuilder()
             .setRayTracer(new SimpleRayTracer(scene))
-            .setLocation(Point.ZERO).setDirection(new Vector(0, 0, -1), new Vector(0, 1, 0))
+            .setLocation(Point.ZERO).setDirection(new Point(0, 0, -1), Vector.Y)
             .setVpDistance(100)
             .setVpSize(500, 500);
 
@@ -48,28 +45,52 @@ public class RenderTests {
         camera
                 .setImageWriter(new ImageWriter("base render test", 1000, 1000))
                 .build()
-                .renderImage();
-        camera.
-                build()
-                .printGrid(100, new Color(YELLOW));
-        camera.build().writeToImage();
-
+                .renderImage()
+                .printGrid(100, new Color(YELLOW))
+                .writeToImage();
     }
 
-//   /** Test for XML based scene - for bonus */
-//   @Test
-//   public void basicRenderXml() {
-//      // enter XML file name and parse from XML file into scene object
-//      // using the code you added in appropriate packages
-//      // ...
-//      // NB: unit tests is not the correct place to put XML parsing code
-//
-//      camera
-//         .setImageWriter(new ImageWriter("xml render test", 1000, 1000))
-//         .build()
-//         .renderImage()
-//         .printGrid(100, new Color(java.awt.Color.YELLOW))
-//         .writeToImage();
-//   }
-}
+    // For stage 6 - please disregard in stage 5
+    /**
+     * Produce a scene with basic 3D model - including individual lights of the
+     * bodies and render it into a png image with a grid
+     */
+    @Test
+    public void renderMultiColorTest() {
+        scene.geometries.add( // center
+                new Sphere(new Point(0, 0, -100), 50),
+                // up left
+                new Triangle(new Point(-100, 0, -100), new Point(0, 100, -100), new Point(-100, 100, -100))
+                        .setEmission(new Color(GREEN)),
+                // down left
+                new Triangle(new Point(-100, 0, -100), new Point(0, -100, -100), new Point(-100, -100, -100))
+                        .setEmission(new Color(RED)),
+                // down right
+                new Triangle(new Point(100, 0, -100), new Point(0, -100, -100), new Point(100, -100, -100))
+                        .setEmission(new Color(BLUE)));
+        scene.setAmbientLight(new AmbientLight(new Color(WHITE), new Double3(0.2, 0.2, 0.2))); //
 
+        camera
+                .setImageWriter(new ImageWriter("color render test", 1000, 1000))
+                .build()
+                .renderImage()
+                .printGrid(100, new Color(WHITE))
+                .writeToImage();
+    }
+
+    /** Test for XML based scene - for bonus */
+    @Test
+    public void basicRenderXml() {
+        // enter XML file name and parse from XML file into scene object
+        // using the code you added in appropriate packages
+        // ...
+        // NB: unit tests is not the correct place to put XML parsing code
+
+        camera
+                .setImageWriter(new ImageWriter("xml render test", 1000, 1000))
+                .build()
+                .renderImage()
+                .printGrid(100, new Color(YELLOW))
+                .writeToImage();
+    }
+}
